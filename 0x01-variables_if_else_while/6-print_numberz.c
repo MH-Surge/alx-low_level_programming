0b71d476b60afdfb901100cdc80467c88d6a3c51
Mark Hiwa <surgemwltd@gmail.com>
2022-07-04 00:27:10 -0700

    numberzzzzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * * Main - Print alphabet in lowercase followed by a new line using putchar
+ *  * Description: program prints the alphabet in lowercase followed by new line
+ *   * Return: Always 0
+ *    *
+ *     */
+int main(void)
+{
+	  int n;
+	    for (n = 0; n < 10; n+++)
+	    {
+		    printf("%d", n);
+	    } 
+	    printf("\n");
+	      return (0);
+}
