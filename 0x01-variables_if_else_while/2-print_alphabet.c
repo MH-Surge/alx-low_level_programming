bc70690c172897d00149e1f3c0c3059b3405985d
Mark Hiwa <surgemwltd@gmail.com>
2022-07-03 23:01:02 -0700

    print lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main - Print alphabet in lowercase followed by a new line using putchar
+ * Description: program prints the alphabet in lowercase followed by new line
+ * Return: Always 0
+ *
+*/
+
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 112; ch++)
+
+	{
+		putchar(ch) :
+	}
+	putchar(10)
+
+	return (0);
+}
