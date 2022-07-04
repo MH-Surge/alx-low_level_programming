1d197d8c48ff36d74a22863f72c3a973656630db
Mark Hiwa <surgemwltd@gmail.com>
2022-07-04 00:38:52 -0700

    #include <stdlib.h> #include <time.h> #include <stdio.h> /** * Main - Print alphabet in lowercase followed by a new line using putchar  * Description: program prints the alphabet in lowercase followed by new line  * Return: Always 0  *  */ int main(void) {   int ch;   for (ch = 97; ch <= 112; ch++) } for (ch = 65; ch <= 90; ch++) {   putchar(ch) : } putchar(10) return (0); }

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * * Main - Print alphabet in lowercase followed by a new line using putchar
+ *  * Description: program prints the alphabet in lowercase followed by new line
+ *   * Return: Always 0
+ *    *
+ *     */
+int main(void)
+{
+	  char ch;
+	    for (ch = 'z'; ch >= 'a'; ch++)
+	    {
+		    putchar(ch)
+	    }
+putchar(10)
+	return (0);
+	}
