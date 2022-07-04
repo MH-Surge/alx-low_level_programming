f35cd46e8c6bcd55edb7b24fab0b03c6c5147d26
Mark Hiwa <surgemwltd@gmail.com>
2022-07-03 23:27:15 -0700

    alphaBET(S)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * * Main - Print alphabet in lowercase followed by a new line using putchar
+ *  * Description: program prints the alphabet in lowercase followed by new line
+ *   * Return: Always 0
+ *    *
+ *     */
+int main(void)
+{
+	  int ch;
+	    for (ch = 97; ch <= 112; ch++)
+}
+for (ch = 65; ch <= 90; ch++)
+{
+	  putchar(ch) :
+}
+putchar(10)
+	return (0);
+	}
